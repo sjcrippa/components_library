7587c3902af21e29c075b03ccce6ad5c367367e6
Santiago Crippa <santicrippa@gmail.com>
2023-11-27 12:21:24 -0300

    fixing bug

--- src/app/components/library/Button.tsx
+++ src/app/components/library/Button.tsx
@@ -6,7 +6,28 @@ import '../../globals.css'
 export default function ButtonComp() {
   const { theme, toggleTheme } = useTheme()
   console.log('Tema actual:', theme)
-  
+
+  const getButtonStyles = (selectedTheme: string) => {
+    let buttonStyle = {
+      padding: '0.75rem',
+      border: '1px solid #000',
+      borderRadius: '0.25rem',
+      transition: 'all 1s ease',
+    } as React.CSSProperties;
+
+    if (selectedTheme === 'smooth') {
+      buttonStyle.backgroundColor = 'yellow';
+      buttonStyle.color = 'red'
+      
+    } else if (selectedTheme === 'future') {
+      buttonStyle.backgroundColor = 'blue';
+    }
+
+    return buttonStyle;
+  };
+
+  const buttonStyles = getButtonStyles(theme);
+
   return (
     <>
       <div className="w-full h-full">
@@ -14,9 +35,7 @@ export default function ButtonComp() {
           <span>Style: {theme}</span>
         </button>
         <div className="flex justify-center mt-36">
-          <button
-            className={`${theme === 'smooth' ? 'smooth' : 'future'} p-3 border border-primary rounded transition-all`}
-          >
+          <button style={buttonStyles}>
             Button
           </button>
         </div>
---
+++ src/types/stylesTypes.ts
@@ -0,0 +1,7 @@
+export interface ButtonStyle {
+  padding: string;
+  border: string;
+  borderRadius: string;
+  transition: string;
+  backgroundColor?: string; // Propiedad backgroundColor opcional
+};
